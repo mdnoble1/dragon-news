166376ff191fe24c745e034ce5361f9662e7d725
Md Noble <mdnoble.wd@gmail.com>
2023-10-06 12:43:41 +0600

    added right side navbar login section

--- src/pages/shared/RightSideNav/RightSideNav.jsx
+++ src/pages/shared/RightSideNav/RightSideNav.jsx
@@ -1,9 +1,22 @@
+import { FaGithub, FaGoogle } from 'react-icons/fa';
+
 const RightSideNav = () => {
-    return (
-        <div>
-            <h2 className="text-3xl">Right Side</h2>
-        </div>
-    );
+  return (
+    <div>
+      {/* login section  */}
+      <div className="px-4 py-2">
+        <h2 className="font-bold text-2xl mb-8">Login With</h2>
+        <button className="btn btn-outline w-full hover:text-blue-500 mb-2">
+            <FaGoogle></FaGoogle>Login With Google</button>
+        <button className="btn btn-outline w-full hover:text-red-500">
+            <FaGithub></FaGithub>Login With Github</button>
+      </div>
+      {/* find us section  */}
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default RightSideNav;
