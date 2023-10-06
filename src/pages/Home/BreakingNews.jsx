21d4cc04942f38ed7faddb86bb68d3714d95892f
Md Noble <mdnoble.wd@gmail.com>
2023-10-06 12:15:32 +0600

    added navbar

--- src/pages/Home/BreakingNews.jsx
+++ src/pages/Home/BreakingNews.jsx
@@ -1,4 +1,5 @@
 import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
 
 const BreakingNews = () => {
   return (
@@ -6,12 +7,12 @@ const BreakingNews = () => {
       <button className="bg-[#D72050] px-6 py-2 font-medium text-xl text-white mr-4">
         Latest
       </button>
-      <div>
-        <Marquee speed={100} className="font-semibold text-xl mr-12">
-          I can be a React component, multiple React components, or just some
-          text...
-        </Marquee>
-      </div>
+      <Marquee speed={100} className="font-semibold text-xl">
+        <Link className="mr-12">I can be a React component, multiple React components, or just some
+        text...</Link>
+        <Link className="mr-12">I can be a React component, multiple React components, or just some
+        text...</Link>
+      </Marquee>
     </div>
   );
 };
--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -1,9 +1,66 @@
+import { NavLink } from "react-router-dom";
+import userDefaultProfile from "../../../assets/user.png";
+
 const Navbar = () => {
-    return (
-        <div>
-            <h2 className="text-3xl">This Is Navbar</h2>
+  const navLinks = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about">About</NavLink>
+      </li>
+      <li>
+        <NavLink to="/career">Career</NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar my-5">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navLinks}
+          </ul>
         </div>
-    );
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 text-lg text-gray-400">
+          {navLinks}
+        </ul>
+      </div>
+      <div className="navbar-end gap-2">
+        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+          <div className="w-10 rounded-full">
+            <img src={userDefaultProfile} />
+          </div>
+        </label>
+        <button className="bg-[#403F3F] px-10 py-2 font-medium text-xl text-white">
+          Login
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
