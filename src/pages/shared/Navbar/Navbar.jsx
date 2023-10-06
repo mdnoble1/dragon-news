bcb50f4685d12658c44377eb84af2c9fc07e4624
Md Noble <mdnoble.wd@gmail.com>
2023-10-06 13:27:11 +0600

    added right side navbar find us and q zone section

--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar my-5">
+    <div className="navbar my-6 p-0">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
--- src/pages/shared/RightSideNav/RightSideNav.jsx
+++ src/pages/shared/RightSideNav/RightSideNav.jsx
@@ -1,4 +1,8 @@
-import { FaGithub, FaGoogle } from 'react-icons/fa';
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from 'react-icons/fa';
+
+import qzone1 from "../../../assets/qZone1.png";
+import qzone2 from "../../../assets/qZone2.png";
+import qzone3 from "../../../assets/qZone3.png";
 
 const RightSideNav = () => {
   return (
@@ -12,8 +16,19 @@ const RightSideNav = () => {
             <FaGithub></FaGithub>Login With Github</button>
       </div>
       {/* find us section  */}
-      <div></div>
-      <div></div>
+      <div className="px-4 py-2 mt-12">
+        <h2 className="font-bold text-2xl mb-8">Find Us On</h2>
+        <a href="" className='font-medium text-lg flex items-center gap-2 border border-gray-600 rounded-t-lg px-6 py-4'><FaFacebook></FaFacebook>Facebook</a>
+        <a href="" className='font-medium text-lg flex items-center gap-2  border-gray-600 border-x px-6 py-4'><FaTwitter></FaTwitter>Twitter</a>
+        <a href="" className='font-medium text-lg flex items-center gap-2 border border-gray-600 rounded-b-lg px-6 py-4'><FaInstagram></FaInstagram>Instagram</a>
+      </div>
+      {/* Q zone section  */}
+      <div className="px-6 py-10 mt-4 bg-gray-300 rounded-sm">
+        <h2 className="font-bold text-3xl mb-8">Q Zone</h2>
+        <img className='w-full mb-8' src={qzone1} alt="" />
+        <img className='w-full mb-8' src={qzone2} alt="" />
+        <img className='w-full' src={qzone3} alt="" />
+      </div>
       <div></div>
     </div>
   );
