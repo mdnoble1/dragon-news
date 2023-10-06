e441d5601a1434332727c930ce1c89fe7ff4816b
Md Noble <mdnoble.wd@gmail.com>
2023-10-06 13:49:52 +0600

    added left side categories dynamically

--- src/pages/shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+
 const LeftSideNav = () => {
-    return (
-        <div>
-            <h2 className="text-3xl">Left Side</h2>
-        </div>
-    );
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
+  return (
+    <div className="px-4 py-2">
+      <h2 className="font-bold text-2xl mb-8">All Category</h2>
+      {categories.map((category) => (
+        <NavLink
+          className="block font-medium text-xl text-gray-400 px-10 py-4"
+          to={`/category/${category.id}`}
+          key={category.id}
+        >
+          {category.name}
+        </NavLink>
+      ))}
+    </div>
+  );
 };
 
 export default LeftSideNav;
