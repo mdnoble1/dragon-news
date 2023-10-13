1c4e4ad3a6a311982bddcc06238b8acd0ab8dd07
Md Noble <mdnoble.wd@gmail.com>
2023-10-13 19:47:19 +0600

    added style on the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -18,13 +18,13 @@ const Home = () => {
             <BreakingNews></BreakingNews>
             <Navbar></Navbar>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-                <div className="border border-red-600"><LeftSideNav></LeftSideNav></div>
-                <div className="md:col-span-2 border border-red-600">
+                <div><LeftSideNav></LeftSideNav></div>
+                <div className="md:col-span-2">
                     {
                         news.map(aNews => <NewsCard key={aNews._id} news={aNews}></NewsCard>)
                     }
                 </div>
-                <div className="border border-red-600"><RightSideNav></RightSideNav></div>
+                <div><RightSideNav></RightSideNav></div>
             </div>
         </div>
     );
--- src/pages/Home/NewsCard.jsx
+++ src/pages/Home/NewsCard.jsx
@@ -1,26 +1,65 @@
 import { Link } from "react-router-dom";
+import { FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from "react-icons/fa";
+import { FaEye } from "react-icons/fa6";
+import Rating from "react-rating";
 
 /* eslint-disable react/prop-types */
-const NewsCard = ( {news} ) => {
+const NewsCard = ({ news }) => {
+  const { title, image_url, details, _id, author, total_view , rating} = news;
 
-    const { title, image_url , details , _id} = news;
+  const date = JSON.stringify(author.published_date);
 
-    return (
-        <div>
-            <div></div>
-            <div>
-                <h2 className="font-bold text-xl text-[#403F3F] mb-5">{title}</h2>
-                <img className="w-full mb-8" src={image_url} alt="" />
-                {
-                    details.length > 200 && <p className="font-semibold  text-[#706F6F]">{details.slice(0 , 200)} ...</p>
-                }
-                <Link to={`/news/${_id}`} className="font-semibold text-[#FF8C47]">Read More</Link>
-                <div className="my-6">
-                    <hr />
-                </div>
-            </div>
+  const dateStr = date.slice(1, 11);
+
+  return (
+    <div className="border border-gray-200 rounded-md mb-5">
+      <div className="flex justify-between items-center bg-gray-200 p-6 rounded-t-md">
+        <div className="flex items-center gap-4">
+          <img className="w-12 rounded-full" src={author.img} alt="" />
+          <div>
+            <h4 className="font-semibold text-[#403F3F] mb-1">{author.name}</h4>
+            <p className="text-sm text-[#706F6F]">{dateStr}</p>
+          </div>
         </div>
-    );
+        <div className="flex items-center gap-4 text-2xl hover:cursor-pointer">
+          <FaRegBookmark></FaRegBookmark>
+          <FaShareAlt></FaShareAlt>
+        </div>
+      </div>
+      <div className="px-6">
+        <h2 className="font-bold text-xl text-[#403F3F] my-5">{title}</h2>
+        <img className="w-full mb-8" src={image_url} alt="" />
+        {details.length > 200 && (
+          <p className="font-semibold  text-[#706F6F]">
+            {details.slice(0, 200)} ...
+          </p>
+        )}
+        <Link to={`/news/${_id}`} className="font-semibold text-[#FF8C47]">
+          Read More
+        </Link>
+        <div className="my-6">
+          <hr />
+        </div>
+      </div>
+      <div className="flex justify-between items-center px-6 mb-5">
+        <div>
+          <div className="flex items-center gap-2">
+            <Rating
+              emptySymbol={<FaRegStar className="text-[#FF8C47] text-2xl"></FaRegStar>}
+              fullSymbol={<FaStar className="text-[#FF8C47] text-2xl"></FaStar>}
+              initialRating={rating.number}
+              readonly
+            />
+            <p className="font-medium text-[#706F6F]">{rating.number}</p>
+          </div>
+        </div>
+        <div className="font-semibold text-[#706F6F] text-xl flex items-center gap-2">
+          <FaEye></FaEye>
+          <p>{total_view}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NewsCard;
