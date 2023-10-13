303fbda18cc3b74d73a78d67df6b06093ac518a7
Md Noble <mdnoble.wd@gmail.com>
2023-10-13 20:59:33 +0600

    added useLocation pathname to continue in page after login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Navbar from "../shared/Navbar/Navbar";
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
@@ -6,6 +6,12 @@ import { AuthContext } from "../../providers/AuthProvider";
 const Login = () => {
 
   const { signInUser } = useContext(AuthContext);
+
+  const location = useLocation();
+
+  const navigate = useNavigate();
+
+  console.log("location in login page ", location);
 
     const handleLogin = e => {
         e.preventDefault();
@@ -19,6 +25,9 @@ const Login = () => {
         signInUser(email , password)
           .then(result => {
             console.log(result.user);
+
+
+            navigate( location?.state ? location.state : "/");
           })
           .catch(error => {
             console.error(error);
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/prop-types */
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
 
     const { user , loading } = useContext(AuthContext);
+
+    const location = useLocation();
+    console.log(location.pathname);
 
     if ( loading ) {
        return <div className="h-screen flex items-center justify-center">
@@ -18,7 +21,7 @@ const PrivateRoute = ({ children }) => {
     }
 
 
-    return <Navigate to="/login"></Navigate>
+    return <Navigate state={location.pathname} to="/login"></Navigate>
 };
 
 export default PrivateRoute;
